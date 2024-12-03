63863d5acb9924e1b4ec3d6d4c29a3875493dd4d
AndresRivera111 <a.22308051281295@cbtis128.edu.mx>
2024-12-03 11:14:49 -0600

    Add files via upload

--- empleados_app/models.py
+++ empleados_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Empleados(models.Model):
     id_empleado = models.IntegerField(primary_key=True)
     nombre = models.CharField(max_length=40)
-    edad = models.IntegerField()
+    edad = models.DateField()
     rfc = models.CharField(max_length=40)
     curp = models.CharField(max_length=40)
     correo_electronico = models.CharField(max_length=20)
